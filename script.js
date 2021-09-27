32df527ab359956d38ea713cdcef669dff076eaa
Man0zZ <82900656+Man0zZ@users.noreply.github.com>
2021-09-28 02:25:26 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  /*Active link*/
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+// const sections = document.querySelectorAll("section[id]");
+
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50;
+//     sections = current.getAttribute("id");
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".menu-nav a[href*=" + sections + "]")
+//         .classList.add("active-link");
+//     } else {
+//       document
+//         .querySelector(".menu-nav a[href*=" + sections + "]")
+//         .classList.remove("active-link");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home-title", {});
+sr.reveal(".btn", { delay: 200 });
+sr.reveal(".home-img", { delay: 400 });
+sr.reveal(".home-social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about-img", {});
+sr.reveal(".about-subtitle", { delay: 400 });
+sr.reveal(".about-text", { delay: 400 });
+sr.reveal(".section-title", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills-subtitle", {});
+sr.reveal(".skills-text", {});
+sr.reveal(".skills-data", { interval: 200 });
+sr.reveal(".skills-img", { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal(".work-img", { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact-input", { interval: 200 });
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
